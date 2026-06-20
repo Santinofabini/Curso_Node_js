4f907ff698b0de7609b6512448dac68ccf0fc2e4
santino <santino.faabini3@gmail.com>
2026-06-20 15:51:19 -0300

    CommnJS

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log('Hello, World!');
+const sum = require('./sum'); //se copia en sum lo exportado
+console.log(sum(2,3));
---
+++ sum.js
@@ -0,0 +1,4 @@
+function sum(a,b)  {
+    return a + b;
+}   
+module.exports = sum; //se exporta la función sum para que pueda ser usada en otros archivos
