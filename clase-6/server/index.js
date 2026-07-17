a15adafe4c5dc3b6774090da3faf3a261e7f0694
santino <santino.faabini3@gmail.com>
2026-07-17 14:22:28 -0300

    second

--- clase-6/server/index.js
+++ clase-6/server/index.js
@@ -2,25 +2,48 @@ import express from 'express'
 import logger from 'morgan'
 import {Server} from 'socket.io'
 import { createServer } from 'node:http'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
 
-
+dotenv.config()
 
 const PORT = process.env.PORT ?? 3000
 const app = express()
 const server = createServer(app)
 const io = new Server(server)
 
-io.on('connection', () => {
-    console.log('New client connected')
+const db = createClient({
+    url: "libsql://capable-leatherhead-santinofabini.aws-us-east-1.turso.io",
+    authToken: process.env.DB_TOKEN
 })
 
+await db.execute("CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT)")
 
+io.on('connection', (socket) => {
+    console.log('New client connected')
+    
+    socket.on('disconnect', () => {
+        console.log('Client disconnected')
+    })
+
+    socket.on('message', async (data) => {
+        let result
+        try {
+            result = await db.execute("INSERT INTO messages (content) VALUES (?)", [data])
+        } catch (error) {
+            console.error('Error inserting message:', error)
+        }
+        io.emit('message', data)
+    });
+})
+
+app.use(logger('dev'))
 
 app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.use(logger('dev'))
+
 
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
